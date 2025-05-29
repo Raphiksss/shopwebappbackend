b4b453f54d3796e6b449ca31071df0d72df2cec8
Raphiksss <bibi.tara.leon@gmail.com>
2025-05-29 15:07:25 +0200

    fix Product.py

--- core/models/Product.py
+++ core/models/Product.py
@@ -1,16 +1,12 @@
-from sqlalchemy import Column, LargeBinary, String
+from sqlalchemy import LargeBinary, String, Column
 from sqlalchemy.orm import Mapped
-
-from . import Base
+from .Base import Base
 
 class Product(Base):
     title: Mapped[str]
     description: Mapped[str]
     price: Mapped[int]
-    data = Column(LargeBinary, nullable = False)
-    mimetype = Column(String(50), nullable = False)
-
-
-
+    data: Mapped[bytes] = Column(LargeBinary, nullable=False)
+    mimetype: Mapped[str] = Column(String(50), nullable=False)
 
 
