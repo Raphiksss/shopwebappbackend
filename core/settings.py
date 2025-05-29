d379f99d47ccbb8cb40fa67e2c5ebd8438ab1214
Raphiksss <bibi.tara.leon@gmail.com>
2025-05-29 14:44:41 +0200

    small changes

--- core/settings.py
+++ core/settings.py
@@ -5,6 +5,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     host : str = '127.0.0.52'
     port: int = '1234'
+    bd_url: str = 'sqlite+aiosqlite:///./db.sqlite3'
 
 settings = Settings()
 
