4d9ce2b84cc0a679c855badad4318a59de8014e7
Raphiksss <bibi.tara.leon@gmail.com>
2025-07-18 20:51:49 +0200

    подогнал одну из апишок к рест стандарту

--- api_products/views.py
+++ api_products/views.py
@@ -9,31 +9,31 @@ from core.db_helper import get_session
 from .dependensiec import text_data,upload_foto, get_product_by_id_dp
 
 
-router = APIRouter(tags = ["Products"])
+router = APIRouter(tags = ["Products"], prefix = "/products")
 
-@router.post('/create_product/', summary = "Создать товар с изображением")
+@router.post('/', summary = "Создать товар с изображением")
 async def create_product(product_in: ProductCreate = Depends(text_data),
                          session: AsyncSession = Depends(get_session),
                          file: UploadFile = Depends(upload_foto)):
                         await create_product_crud(session, product_in, file)
                         return "success"
 
-@router.get('/get_products/', summary = "Получение всех товаров", response_model = list[ProductRead])
+@router.get('/', summary = "Получение всех товаров", response_model = list[ProductRead])
 async def get_products(session: AsyncSession = Depends(get_session)):
     products = await get_products_crud(session)
     return products
 
-@router.get('/get_product/{product_id}/', summary = "Получить товар по ID ", response_model = ProductRead )
+@router.get('/{product_id}/', summary = "Получить товар по ID ", response_model = ProductRead )
 async def get_product_by_id(product_id: int,session: AsyncSession = Depends(get_session)):
     product = await get_product_by_id_dp(product_id, session)
     return product
 
-@router.patch('/update_title/', summary = "Обновить название",)
-async def path_title(new_title : str, product: Product = Depends(get_product_by_id_dp), session: AsyncSession = Depends(get_session)):
-    await update_title(session,product, new_title)
-    return "success"
+# @router.patch('/update_title/', summary = "Обновить название",)
+# async def path_title(new_title : str, product: Product = Depends(get_product_by_id_dp), session: AsyncSession = Depends(get_session)):
+#     await update_title(session,product, new_title)
+#     return "success"
 
-@router.delete('/delete_product/{product_id}', summary = "Удалить товар")
+@router.delete('/{product_id}/', summary = "Удалить товар")
 async def delete_item(product:Product = Depends(get_product_by_id_dp), session: AsyncSession = Depends(get_session)):
     await delete_product(session = session, product = product)
     return "success"
