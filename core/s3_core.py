b85236484e6189e164f1f6754642f3a8aabc7433
Raphiksss <bibi.tara.leon@gmail.com>
2026-01-26 15:06:15 +0100

    fixed bug with images #4

--- core/s3_core.py
+++ core/s3_core.py
@@ -51,7 +51,7 @@ class S3Client:
                 ContentType=ctype,
                 ContentDisposition="inline"
             )
-        return f"{settings.DB_Settings.images_uri}/{self.bucket_name}/{object_name}"
+        return f"{settings.DB.images_uri}/{self.bucket_name}/{object_name}"
 
 s3 = S3Client(
         access_key   = MINIO_ROOT_USER,
