b2044a16c6e8d13248603e55186618d870ee8b70
Raphiksss <bibi.tara.leon@gmail.com>
2026-09-03 12:19:11 +0000

    hidden API structure

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ async def lifespan(app: FastAPI):
     logger.info("RabbitMQ broker stopped")
 
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(lifespan=lifespan, docs_url=None, redoc_url=None, openapi_url=None)
 
 app.include_router(v1_router)
 app.add_middleware(
